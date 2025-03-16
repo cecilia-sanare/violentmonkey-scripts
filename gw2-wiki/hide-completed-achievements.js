cfc1c500b4d82f84db8a73c9023f8254e4ffd649
Ceci <ceci@sanare.dev>
2025-03-16 11:27:11 -0500

    feat(gw2-wiki): automatically hide achievements on page load
    - also automatically ping every 10 seconds.

--- gw2-wiki/hide-completed-achievements.js
+++ gw2-wiki/hide-completed-achievements.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name        Hide Completed Achievements
+// @name        wiki.guildwars2.com - Hide Completed Achievements
 // @namespace   Violentmonkey Scripts
 // @match       https://wiki.guildwars2.com/*
 // @grant       none
@@ -8,6 +8,8 @@
 // @description Automatically hide completed achievements
 // @downloadURL https://raw.githubusercontent.com/cecilia-sanare/violentmonkey-scripts/refs/heads/main/gw2-wiki/hide-completed-achievements.js
 // @homepageURL https://github.com/cecilia-sanare/violentmonkey-scripts
+// @require https://cdn.jsdelivr.net/npm/@violentmonkey/dom@2
+// @top-level-await
 // ==/UserScript==
 
 const observer = new MutationObserver((records) => {
@@ -25,3 +27,28 @@ const observer = new MutationObserver((records) => {
 observer.observe(document.head, {
   childList: true,
 });
+
+function waitForElement(selector) {
+  return new Promise((resolve) => {
+    VM.observe(document.body, () => {
+      const node = document.querySelector(selector);
+
+      if (node) {
+        resolve(node);
+        return true;
+      }
+    });
+  });
+}
+
+
+const apply = await waitForElement('#WidgetAccountAchievements-controls > input[value="Apply"]');
+
+if (apply) {
+  apply.click();
+
+  // Check for new achievements every 10 seconds.
+  setInterval(() => apply.click(), 1000 * 10);
+} else {
+  console.warn('[CS]: Failed to find the "Apply" button...');
+}
